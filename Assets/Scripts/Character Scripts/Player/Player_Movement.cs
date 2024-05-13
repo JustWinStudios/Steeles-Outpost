69232ec35257c0893d490ccaf8996681e8e5bdd3
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-05-13 14:29:53 -0400

    Created new player movement script

---
+++ Assets/Scripts/Character Scripts/Player/Player_Movement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+    }
+}

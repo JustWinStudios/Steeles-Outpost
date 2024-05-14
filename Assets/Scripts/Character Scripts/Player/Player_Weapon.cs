e58c14c7d308faf908648f497ed3654f754178df
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-05-14 12:23:22 -0400

    Created player weaopon control script

--- Assets/Scripts/Character Scripts/Player/Player_Weapon.cs
+++ Assets/Scripts/Character Scripts/Player/Player_Weapon.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player_Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Editor Data
+    [Header("Dependencies")]
+    [SerializeField] private Transform weapon;
+    [SerializeField] private float offset;
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    #region Tick
+    private void Update()
     {
-        
+        WeaponRotation();
     }
+    #endregion
+
+    #region Weapon Rotation Logic
+    private void WeaponRotation()
+    {
+        Vector3 displacement = weapon.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        // Calculates the angle between the weapon and mouse cursor
+        float angle = Mathf.Atan2(displacement.y, displacement.x) * Mathf.Rad2Deg;
+
+        weapon.rotation = Quaternion.Euler(0f, 0f, angle + offset);
+    }
+    #endregion
 }

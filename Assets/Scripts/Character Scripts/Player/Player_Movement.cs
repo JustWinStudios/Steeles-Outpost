47a7b8c738c9b2311e34e571871891a728119cfd
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-05-14 11:57:45 -0400

    Updated player movement system

--- Assets/Scripts/Character Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Character Scripts/Player/Player_Movement.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    #region Editor Data
+    [Header("Movement Attributes")]
+    [SerializeField] float _moveSpeed = 100f;
+    [Header("Dependencies")]
+    [SerializeField] Rigidbody2D _rigidbody;
+    #endregion
+
+    #region Internal Data
+    private Vector2 _moveDirection = Vector2.zero;
+    #endregion
+
+    #region Tick
     private void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-
-        }
+        GatherInput();
     }
+
+    private void FixedUpdate()
+    {
+        MovementUpdate();
+    }
+    #endregion
+
+    #region Input Logic
+    private void GatherInput()
+    {
+        _moveDirection.x = Input.GetAxisRaw("Horizontal");
+        _moveDirection.y = Input.GetAxisRaw("Vertical");
+    }
+    #endregion
+
+    #region Movement Logic
+    private void MovementUpdate()
+    {
+        _rigidbody.velocity = _moveDirection * _moveSpeed * Time.fixedDeltaTime;
+    }
+    #endregion 
 }

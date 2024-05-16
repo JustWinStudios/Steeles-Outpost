726554e5c2d2d2cdf0cbf35a1d4b910b1b51bce2
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-05-15 23:14:55 -0400

    Updated the ResourceNodeSpawner

--- Assets/Scripts/World Generation/ResourceNodeSpawner.cs
+++ Assets/Scripts/World Generation/ResourceNodeSpawner.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class ResourceNodeSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Editor Data
+    [Header("Spawner Settings")]
+    [SerializeField] private GameObject[] resourcePrefabs;
+    [SerializeField] private int maxResourceNodes = 50;
+    [SerializeField] private float spawnInterval = 300f; //Time in seconds
+
+    [Header("Spawn Area Settings")]
+    [SerializeField] private Vector2 spawnAreaMin;
+    [SerializeField] private Vector2 spawnAreaMax;
+
+    private float nextSpawnTime;
+    #endregion
+
+    #region Unity Callbacks
+    private void Start()
     {
-        
+        nextSpawnTime = Time.time + spawnInterval;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Time.time >= nextSpawnTime)
+        {
+            SpawnResourceNodes();
+            nextSpawnTime = Time.time +spawnInterval;
+        }
     }
+    #endregion
+
+    #region Resource Spawning Logic
+    private void SpawnResourceNodes()
+    {
+        int currentResourceNodes = GameObject.FindGameObjectsWithTag("ResourceNode").Length;
+        int nodesToSpawn = maxResourceNodes - currentResourceNodes;
+
+        for (int i = 0; i < nodesToSpawn; i++)
+        {
+            Vector2 spawnPosition = new Vector2(
+                Random.Range(spawnAreaMin.x, spawnAreaMax.x),
+                Random.Range(spawnAreaMin.y, spawnAreaMax.y)
+                );
+
+            int randomIndex = Random.Range(0, resourcePrefabs.Length);
+            Instantiate(resourcePrefabs[randomIndex], spawnPosition, Quaternion.identity);
+        }
+    }
+    #endregion
+
+    #region Spawn Area Gizmos
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(new Vector3(spawnAreaMin.x, spawnAreaMin.y, 0), new Vector3(spawnAreaMin.x, spawnAreaMax.y, 0));
+        Gizmos.DrawLine(new Vector3(spawnAreaMin.x, spawnAreaMax.y, 0), new Vector3(spawnAreaMax.x, spawnAreaMax.y, 0));
+        Gizmos.DrawLine(new Vector3(spawnAreaMax.x, spawnAreaMax.y, 0), new Vector3(spawnAreaMax.x, spawnAreaMin.y, 0));
+        Gizmos.DrawLine(new Vector3(spawnAreaMax.x, spawnAreaMin.y, 0), new Vector3(spawnAreaMin.x, spawnAreaMin.y, 0));
+    }
+    #endregion
 }

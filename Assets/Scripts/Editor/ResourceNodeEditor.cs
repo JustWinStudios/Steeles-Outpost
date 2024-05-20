ca524758504fa3497a94287f19f65147ab60e461
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-05-20 18:14:20 -0500

    Update ResourceNodeEditor.cs

--- Assets/Scripts/Editor/ResourceNodeEditor.cs
+++ Assets/Scripts/Editor/ResourceNodeEditor.cs
@@ -1,18 +1,34 @@
-using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class ResourceNodeEditor : MonoBehaviour
+[CustomEditor(typeof(ResourceNode))]
+public class ResourceNodeEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnInspectorGUI()
     {
-        
-    }
+        ResourceNode resourceNode = (ResourceNode)target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (InventoryManager.Instance != null && InventoryManager.Instance.resources.Count > 0)
+        {
+            List<string> resourceNames = new List<string>();
+            foreach (var resource in InventoryManager.Instance.resources)
+            {
+                resourceNames.Add(resource.resourceName);
+            }
+
+            resourceNode.resourceIndex = EditorGUILayout.Popup("Resource", resourceNode.resourceIndex, resourceNames.ToArray());
+        }
+        else
+        {
+            EditorGUILayout.LabelField("No resources available. Please add resources to the InventoryManager.");
+        }
+
+        resourceNode.resourceAmount = EditorGUILayout.IntField("Resource Amount", resourceNode.resourceAmount);
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(resourceNode);
+        }
     }
 }
